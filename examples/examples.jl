c45be4d7a610d85e91e898f26064bc32678810a3
C. Brenhin Keller <cbkeller@princeton.edu>
2018-04-09 01:16:56 -0700

    Remove unnecessary intro from examples.jl

--- examples/examples.jl
+++ examples/examples.jl
@@ -1,45 +1,7 @@
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-#                                   Chron.jl                                    #
-#                                                                               #
-#       A two-part framework for (1) estimating eruption/deposition age         #
-#  distributions from complex mineral age spectra and (2) subsequently building #
-#  a stratigraphic age model based on those distributions. Each step relies on  #
-#  a Markov-Chain Monte Carlo model.                                            #
-#                                                                               #
-#    The first model uses an informative prior distribution to estimate the     #
-#  times of first (i.e., saturation) and last  mineral crystallization (i.e.,   #
-#  eruption/deposition).                                                        #
-#                                                                               #
-#    The second model uses the estimated (posterior) eruption/deposition ages   #
-#  distributions along with the constraint of stratigraphic superposition to    #
-#  produce an age-depth model                                                   #
-#                                                                               #
-#   Last modified by C. Brenhin Keller 2018-04-09                               #
-#                                                                               #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-## --- Load external resources
 
-    # # Basic statistics and UI resources
-    # using StatsBase: fit, Histogram
-    # using ProgressMeter: @showprogress
-    # using LsqFit: curve_fit
-    # using KernelDensity: kde
-    #
-    # # Weighted mean, etc
-    # include("Utilities.jl");
-    # # Functions for estimating extrema of a finite-range distribution
-    # include("DistMetropolis.jl");
-    # # Functions for stratigraphic modelling
-    # include("StratMetropolis.jl");
-    #
-    # # Higher-level functions for fitting and plotting
-    # using Plots, Colors;
-    # gr();
-    # include("Fitplot.jl");
-
-
+## --- Load the Chron package
+    using Plots, Colors
     using Chron
-
 
 ## --- Define sample properties
 
