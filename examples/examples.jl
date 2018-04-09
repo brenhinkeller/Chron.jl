eb3dda51e44d41c1a035c2b6773cfab30f9bb44e
C. Brenhin Keller <cbkeller@princeton.edu>
2018-04-09 02:47:13 -0700

    Add an introduction to examples.jl

--- examples/examples.jl
+++ examples/examples.jl
@@ -1,15 +1,35 @@
-
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                 examples.jl                                   #
+#                                                                               #
+#     Illustrates the use of the Chron.jl package for eruption/deposition       #
+#  age estimation and production of a stratigraphic age model.                  #
+#                                                                               #
+#     This file uses code cells (denoted by "## ---"). To evaluate a cell in    #
+#  the Julia REPL and move to the next cell, the default shortcut in Atom is    #
+#  alt-shift-enter.                                                             #
+#                                                                               #
+#      You may have to adjust the path below which specifies the location of    #
+#  the CSV data files for each sample (from examples/DenverUPbExampleData/      #
+#  in the Chron.jl repository).                                                 #
+#                                                                               #
+#   Last modified by C. Brenhin Keller 2018-04-09                               #
+#                                                                               #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 ## --- Load the Chron package
-    using Plots, Colors
+
     using Chron
 
 ## --- Define sample properties
 
 # # # # # # # # # # # # Enter sample information here! # # # # # # # # # # # # #
+    # This example data is from Clyde et al. (2016) "Direct high-precision
+    # U–Pb geochronology of the end-Cretaceous extinction and calibration of
+    # Paleocene astronomical timescales" EPSL 452, 272–280.
+    # doi: 10.1016/j.epsl.2016.07.041
     Name   =        ("KJ08-157", "KJ04-75", "KJ09-66", "KJ04-72", "KJ04-70");
     Height =        [     -52.0,      44.0,      54.0,      82.0,      93.0];
     Height_Sigma =  [       3.0,       1.0,       3.0,       3.0,       3.0];
-    inputSigmaLevel = 2; # i.e., are the data files 1-sigma or 2-sigma
+    inputSigmaLevel = 2; # i.e., are the data files 1-sigma or 2-sigma. Integer.
     Path = "examples/DenverUPbExampleData/" # Where are the data files?
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
@@ -35,7 +55,7 @@
     # Bootstrap a KDE of the pre-eruptive (or pre-deposition) zircon distribution
     # shape from individual sample datafiles using a KDE of stacked sample data
     BootstrappedDistribution = BootstrapDistributionKDE(smpl);
-    plot(BootstrappedDistribution,xlabel="Time (unitless)",ylabel="")
+    plot(BootstrappedDistribution,xlabel="Time (arbitrary units)",ylabel="Probability Density")
 
 ## --- Estimate the eruption age distributions for each sample
 
@@ -81,24 +101,26 @@
     );
 
     # Run the model
-    (mdl, agedist, lldist) = StratMetropolis(smpl, config);
+    (mdl, agedist, lldist) = StratMetropolisDist(smpl, config);
 
     # Plot results (mean and 95% confidence interval for both model and data)
+    using Plots
+    gr()
     hdl = plot([mdl.Age_025CI; reverse(mdl.Age_975CI)],[mdl.Height; reverse(mdl.Height)], fill=(minimum(mdl.Height),0.5,:blue), label="model")
     plot!(hdl, mdl.Age, mdl.Height, linecolor=:blue, label="")
     plot!(hdl, smpl.Age, smpl.Height, xerror=(smpl.Age-smpl.Age_025CI,smpl.Age_975CI-smpl.Age),label="data",seriestype=:scatter,color=:black)
     plot!(hdl, xlabel="Age (Ma)", ylabel="Height (cm)")
     savefig(hdl,"AgeDepthModel.pdf");
+    display(hdl)
 
-## --- Same but with hiata
+## --- Same but with hiatus
 
     # # A type of object to hold data about hiatuses
     # hiatus = HiatusData(
-    #     [20.0,], # Height
-    #     [0.0,], # Height_Sigma
-    #     [0.5,], # Duration
-    #     [0.1,], # Duration_Sigma
+    #     [20.0,40.0], # Height
+    #     [0.0,0.0], # Height_Sigma
+    #     [0.5,0.9], # Duration -- this is a minimum duration only
+    #     [0.1,0.05], # Duration_Sigma (one-sided, minimum only)
     # );
     #
     # (mdl, agedist, hiatusdist, lldist) = StratMetropolisHiatus(smpl, hiatus, config);
-    # smpl
