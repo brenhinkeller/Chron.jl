bb838e897989d38d333074edcde21850a5886f69
C. Brenhin Keller <cbkeller@berkeley.edu>
2018-10-26 10:54:25 -0700

    Cleanup extraneous ;

--- src/Fitplot.jl
+++ src/Fitplot.jl
@@ -1,10 +1,10 @@
 ## --- Utility functions for plotting
 
     function plot_rankorder_errorbar(data,uncert; seriestype=:scatter,ylabel="",label="",xticks=[],xlabel="")
-        sI = sortperm(data);
+        sI = sortperm(data)
         h = plot(1:length(sI),data[sI],yerror=uncert[sI],seriestype=seriestype,
                  label=label,ylabel=ylabel,xlabel=xlabel,xticks=xticks)
-        return h;
+        return h
     end
 
 ## -- Bootstrap prior distribution shape
@@ -18,25 +18,25 @@
             data = readdlm("$(smpl.Path)$(smpl.Name[i]).csv",',')
 
             # Maximum extent of expected analytical tail (beyond eruption/deposition)
-            maxTailLength = mean(data[:,2]) ./ smpl.inputSigmaLevel .* norm_quantile(1 - 1/(1+size(data,1)));
-            included = (data[:,1] .- minimum(data[:,1])) .>= maxTailLength;
-            included .|= data[:,1] .> nanmedian(data[:,1]); # Don't exclude more than half (could only happen in underdispersed datasets)
-            included .&= .~isnan.(data[:,1]); # Exclude NaNs
+            maxTailLength = mean(data[:,2]) ./ smpl.inputSigmaLevel .* norm_quantile(1 - 1/(1+size(data,1)))
+            included = (data[:,1] .- minimum(data[:,1])) .>= maxTailLength
+            included .|= data[:,1] .> nanmedian(data[:,1]) # Don't exclude more than half (could only happen in underdispersed datasets)
+            included .&= .~isnan.(data[:,1]) # Exclude NaNs
 
             # Include and scale only those data not within the expected analytical tail
             if sum(included)>0
-                scaled = data[included,1] .- minimum(data[included,1]);
+                scaled = data[included,1] .- minimum(data[included,1])
                 if maximum(scaled) > 0
-                    scaled = scaled ./ maximum(scaled);
+                    scaled = scaled ./ maximum(scaled)
                 end
                 allscaled = [allscaled; scaled]
             end
         end
 
         # Calculate kernel density estimate, truncated at 0
-        kd = kde(allscaled,npoints=2^7);
-        t = kd.x .> -0.05; # Ensure sharp cutoff at eruption / deposition
-        return kd.density[t];
+        kd = kde(allscaled,npoints=2^7)
+        t = kd.x .> -0.05 # Ensure sharp cutoff at eruption / deposition
+        return kd.density[t]
     end
 
     # Bootstrap a KDE of the pre-eruptive (or pre-deposition) mineral crystallization
@@ -45,74 +45,74 @@
         # Load all data points and scale from 0 to 1
         allscaled = Array{Float64,1}([])
         for i=1:size(data,2)
-            scaled = data[:,i]-minimum(data[:,i]);
+            scaled = data[:,i]-minimum(data[:,i])
             if maximum(scaled) > 0
-                scaled = scaled./maximum(scaled);
+                scaled = scaled./maximum(scaled)
             end
             allscaled = [allscaled; scaled]
         end
 
         # Calculate kernel density estimate, truncated at 0
-        kd = kde(allscaled,npoints=2^7);
-        t = kd.x .> -0.05;
-        return kd.density[t];
+        kd = kde(allscaled,npoints=2^7)
+        t = kd.x .> -0.05
+        return kd.density[t]
     end
 
 
 ## --- Fit and plot results from stationary distribution of depostion/eruption age distribution model
 
     # Process and fit stationary distribution for eruption age
-    function tMinDistMetropolis(smpl::StratAgeData,nsteps::Int,burnin::Int,dist::Array{Float64});
+    function tMinDistMetropolis(smpl::StratAgeData,nsteps::Int,burnin::Int,dist::Array{Float64})
 
         # Estimate the distribution for each sample
-        print("Estimating eruption/deposition age distributions...\n");
+        print("Estimating eruption/deposition age distributions...\n")
         for i=1:length(smpl.Name)
             # Load data for each sample
             data = readdlm("$(smpl.Path)$(smpl.Name[i]).csv",',')
-            print(i, ": ", smpl.Name[i], "\n"); # Display progress
+            print(i, ": ", smpl.Name[i], "\n") # Display progress
 
             # Run MCMC to estimate saturation and eruption/deposition age distributions
-            (tminDist, tmaxDist, llDist, acceptanceDist) = metropolis_minmax_cryst(nsteps,dist,data[:,1],data[:,2]/smpl.inputSigmaLevel);
+            (tminDist, tmaxDist, llDist, acceptanceDist) = metropolis_minmax_cryst(nsteps,dist,data[:,1],data[:,2]/smpl.inputSigmaLevel)
 
             # Fill in the strat sample object with our new results
-            smpl.Age[i] = mean(tminDist[burnin:end]);
-            smpl.Age_Sigma[i] = std(tminDist[burnin:end]);
-            smpl.Age_025CI[i] = percentile(tminDist[burnin:end],2.5);
-            smpl.Age_975CI[i] = percentile(tminDist[burnin:end],97.5);
+            smpl.Age[i] = mean(tminDist[burnin:end])
+            smpl.Age_Sigma[i] = std(tminDist[burnin:end])
+            smpl.Age_025CI[i] = percentile(tminDist[burnin:end],2.5)
+            smpl.Age_975CI[i] = percentile(tminDist[burnin:end],97.5)
 
             # Fit custom many-parametric distribution function to histogram
-            edges = linsp(minimum(tminDist[burnin:end]),maximum(tminDist[burnin:end]),101); # Vector of bin edges
+            edges = linsp(minimum(tminDist[burnin:end]),maximum(tminDist[burnin:end]),101) # Vector of bin edges
             hobj = fit(Histogram,tminDist[burnin:end],edges,closed=:left) # Fit histogram object
 
-            t = hobj.weights.>0; # Only look at bins with one or more results
-            N = hobj.weights[t] ./ length(tminDist[burnin:end]) .* length(t); # Normalized number of MCMC steps per bin
-            bincenters = cntr(hobj.edges[1])[t]; # Vector of bin centers
+            t = hobj.weights.>0 # Only look at bins with one or more results
+            N = hobj.weights[t] ./ length(tminDist[burnin:end]) .* length(t) # Normalized number of MCMC steps per bin
+            bincenters = cntr(hobj.edges[1])[t] # Vector of bin centers
 
             # Initial guess for parameters
-            p = ones(5);
-            p[1] = maximum(N);
-            p[2] = mean(tminDist[burnin:end]);
-            p[3] = std(tminDist[burnin:end]);
+            p = ones(5)
+            p[1] = maximum(N)
+            p[2] = mean(tminDist[burnin:end])
+            p[3] = std(tminDist[burnin:end])
 
             # Fit nonlinear model
-            fobj = curve_fit(bilinear_exponential,bincenters,N,p);
-            smpl.Params[:,i] = fobj.param;
+            fobj = curve_fit(bilinear_exponential,bincenters,N,p)
+            smpl.Params[:,i] = fobj.param
 
             # Rank-order plot of analyses and eruption/deposition age range
-            nAnalyses = length(data[:,1]);
+            nAnalyses = length(data[:,1])
             h1 = plot_rankorder_errorbar(data[:,1],2*data[:,2]/smpl.inputSigmaLevel,ylabel="Age (Ma)",label="Ages")
-            m = ones(nAnalyses).*smpl.Age[i];
-            l = ones(nAnalyses).*smpl.Age_025CI[i];
-            u = ones(nAnalyses).*smpl.Age_975CI[i];
-            plot!(h1,1:nAnalyses,l,fillto=u,fillalpha=0.6,linealpha=0, label="Model interpretation");
-            plot!(h1,1:nAnalyses,m,linecolor=:black,linestyle=:dot,label="",legend=:topleft);
-            savefig(h1,string(smpl.Path,smpl.Name[i],"_rankorder.pdf"));
+            m = ones(nAnalyses).*smpl.Age[i]
+            l = ones(nAnalyses).*smpl.Age_025CI[i]
+            u = ones(nAnalyses).*smpl.Age_975CI[i]
+            plot!(h1,1:nAnalyses,l,fillto=u,fillalpha=0.6,linealpha=0, label="Model interpretation")
+            plot!(h1,1:nAnalyses,m,linecolor=:black,linestyle=:dot,label="",legend=:topleft)
+            savefig(h1,string(smpl.Path,smpl.Name[i],"_rankorder.pdf"))
 
             # Plot model fit to histogram
-            h2 = plot(bincenters,N,label="Histogram");
-            plot!(h2,bincenters,bilinear_exponential(bincenters,smpl.Params[:,i]),label="Curve fit");
-            plot!(h2,legend=:topleft,xlabel="Age",ylabel="Likelihood");
-            savefig(h2,string(smpl.Path,smpl.Name[i],"_distribution.pdf"));
+            h2 = plot(bincenters,N,label="Histogram")
+            plot!(h2,bincenters,bilinear_exponential(bincenters,smpl.Params[:,i]),label="Curve fit")
+            plot!(h2,legend=:topleft,xlabel="Age",ylabel="Likelihood")
+            savefig(h2,string(smpl.Path,smpl.Name[i],"_distribution.pdf"))
 
         end
 
@@ -122,57 +122,57 @@
 ## ---
 
     # Process and fit stationary distribution for eruption age
-    function tMinDistMetropolisLA(smpl::StratAgeData,nsteps::Int,burnin::Int,dist::Array{Float64});
+    function tMinDistMetropolisLA(smpl::StratAgeData,nsteps::Int,burnin::Int,dist::Array{Float64})
 
         # Estimate the distribution for each sample
-        print("Estimating eruption/deposition age distributions...\n");
+        print("Estimating eruption/deposition age distributions...\n")
         for i=1:length(smpl.Name)
             # Load data for each sample
             data = readdlm("$(smpl.Path)$(smpl.Name[i]).csv",',')
-            print(i, ": ", smpl.Name[i], "\n"); # Display progress
+            print(i, ": ", smpl.Name[i], "\n") # Display progress
 
             # Run MCMC to estimate saturation and eruption/deposition age distributions
-            (tminDist, tmaxDist, llDist, acceptanceDist) = metropolis_minmax_cryst_LA(nsteps,dist,data[:,1],data[:,2]/smpl.inputSigmaLevel);
+            (tminDist, tmaxDist, llDist, acceptanceDist) = metropolis_minmax_cryst_LA(nsteps,dist,data[:,1],data[:,2]/smpl.inputSigmaLevel)
 
             # Fill in the strat sample object with our new results
-            smpl.Age[i] = mean(tminDist[burnin:end]);
-            smpl.Age_Sigma[i] = std(tminDist[burnin:end]);
-            smpl.Age_025CI[i] = percentile(tminDist[burnin:end],2.5);
-            smpl.Age_975CI[i] = percentile(tminDist[burnin:end],97.5);
+            smpl.Age[i] = mean(tminDist[burnin:end])
+            smpl.Age_Sigma[i] = std(tminDist[burnin:end])
+            smpl.Age_025CI[i] = percentile(tminDist[burnin:end],2.5)
+            smpl.Age_975CI[i] = percentile(tminDist[burnin:end],97.5)
 
             # Fit custom many-parametric distribution function to histogram
-            edges = linsp(minimum(tminDist[burnin:end]),maximum(tminDist[burnin:end]),101); # Vector of bin edges
+            edges = linsp(minimum(tminDist[burnin:end]),maximum(tminDist[burnin:end]),101) # Vector of bin edges
             hobj = fit(Histogram,tminDist[burnin:end],edges,closed=:left) # Fit histogram object
 
-            t = hobj.weights.>0; # Only look at bins with one or more results
-            N = hobj.weights[t] ./ length(tminDist[burnin:end]) .* length(t); # Normalized number of MCMC steps per bin
-            bincenters = cntr(hobj.edges[1])[t]; # Vector of bin centers
+            t = hobj.weights.>0 # Only look at bins with one or more results
+            N = hobj.weights[t] ./ length(tminDist[burnin:end]) .* length(t) # Normalized number of MCMC steps per bin
+            bincenters = cntr(hobj.edges[1])[t] # Vector of bin centers
 
             # Initial guess for parameters
-            p = ones(5);
-            p[1] = maximum(N);
-            p[2] = mean(tminDist[burnin:end]);
-            p[3] = std(tminDist[burnin:end]);
+            p = ones(5)
+            p[1] = maximum(N)
+            p[2] = mean(tminDist[burnin:end])
+            p[3] = std(tminDist[burnin:end])
 
             # Fit nonlinear model
-            fobj = curve_fit(bilinear_exponential,bincenters,N,p);
-            smpl.Params[:,i] = fobj.param;
+            fobj = curve_fit(bilinear_exponential,bincenters,N,p)
+            smpl.Params[:,i] = fobj.param
 
             # Rank-order plot of analyses and eruption/deposition age range
-            nAnalyses = length(data[:,1]);
+            nAnalyses = length(data[:,1])
             h1 = plot_rankorder_errorbar(data[:,1],2*data[:,2]/smpl.inputSigmaLevel,ylabel="Age (Ma)",label="Ages")
-            m = ones(nAnalyses).*smpl.Age[i];
-            l = m - 2*smpl.Age_Sigma[i];
-            u = m + 2*smpl.Age_Sigma[i];
-            plot!(h1,1:nAnalyses,l,fillto=u,fillalpha=0.6,linealpha=0, label="Model interpretation");
-            plot!(h1,1:nAnalyses,m,linecolor=:black,linestyle=:dot,label="");
-            savefig(h1,string(smpl.Path,smpl.Name[i],"_rankorder.pdf"));
+            m = ones(nAnalyses).*smpl.Age[i]
+            l = m - 2*smpl.Age_Sigma[i]
+            u = m + 2*smpl.Age_Sigma[i]
+            plot!(h1,1:nAnalyses,l,fillto=u,fillalpha=0.6,linealpha=0, label="Model interpretation")
+            plot!(h1,1:nAnalyses,m,linecolor=:black,linestyle=:dot,label="")
+            savefig(h1,string(smpl.Path,smpl.Name[i],"_rankorder.pdf"))
 
             # Plot model fit to histogram
-            h2 = plot(bincenters,N,label="Histogram");
-            plot!(h2,bincenters, bilinear_exponential(bincenters,smpl.Params[:,i]),label="Curve fit");
-            plot!(h2,legend=:topleft,xlabel="Age",ylabel="Likelihood");
-            savefig(h2,string(smpl.Path,smpl.Name[i],"_distribution.pdf"));
+            h2 = plot(bincenters,N,label="Histogram")
+            plot!(h2,bincenters, bilinear_exponential(bincenters,smpl.Params[:,i]),label="Curve fit")
+            plot!(h2,legend=:topleft,xlabel="Age",ylabel="Likelihood")
+            savefig(h2,string(smpl.Path,smpl.Name[i],"_distribution.pdf"))
 
         end
 
--- src/Utilities.jl
+++ src/Utilities.jl
@@ -11,50 +11,50 @@
 
     # Calculate a weigted mean, including MSWD, with MSWD correction to uncertainty.
     function gwmean(x, sigma)
-        n = length(x);
-        s1 = 0.0; s2 = 0.0; s3 = 0.0;
+        n = length(x)
+        s1 = 0.0; s2 = 0.0; s3 = 0.0
 
         if n==1
-            wx = x[1];
-            mswd = 0;
-            wsigma = sigma[1];
+            wx = x[1]
+            mswd = 0
+            wsigma = sigma[1]
         else
             for i=1:n
-                s1 += x[i] / (sigma[i]*sigma[i]);
-                s2 += 1 / (sigma[i]*sigma[i]);
+                s1 += x[i] / (sigma[i]*sigma[i])
+                s2 += 1 / (sigma[i]*sigma[i])
             end
-            wx = s1/s2;
+            wx = s1/s2
 
             for i=1:n
-                s3 += (x[i] - wx) * (x[i] - wx) / (sigma[i]*sigma[i]);
+                s3 += (x[i] - wx) * (x[i] - wx) / (sigma[i]*sigma[i])
             end
-            mswd = s3 / (n-1);
-            wsigma = sqrt(mswd/s2);
+            mswd = s3 / (n-1)
+            wsigma = sqrt(mswd/s2)
         end
         return wx, wsigma, mswd
     end
 
     # Calculate a weigted mean, including MSWD, but without MSWD correction to uncertainty
     function awmean(x, sigma)
-        n = length(x);
-        s1 = 0.0; s2 = 0.0; s3 = 0.0;
+        n = length(x)
+        s1 = 0.0; s2 = 0.0; s3 = 0.0
 
         if n==1
-            wx = x[1];
-            mswd = 0;
-            wsigma = sigma[1];
+            wx = x[1]
+            mswd = 0
+            wsigma = sigma[1]
         else
             for i=1:n
-                s1 += x[i] / (sigma[i]*sigma[i]);
-                s2 += 1 / (sigma[i]*sigma[i]);
+                s1 += x[i] / (sigma[i]*sigma[i])
+                s2 += 1 / (sigma[i]*sigma[i])
             end
-            wx = s1/s2;
+            wx = s1/s2
 
             for i=1:n
-                s3 += (x[i] - wx) * (x[i] - wx) / (sigma[i]*sigma[i]);
+                s3 += (x[i] - wx) * (x[i] - wx) / (sigma[i]*sigma[i])
             end
-            mswd = s3 / (n-1);
-            wsigma = sqrt(1.0/s2);
+            mswd = s3 / (n-1)
+            wsigma = sqrt(1.0/s2)
         end
         return wx, wsigma, mswd
     end
@@ -118,21 +118,21 @@
     # defining the PDF curve
     function draw_from_distribution(dist::Array{Float64}, n::Int)
         # Draw n random numbers from the distribution 'dist'
-        x = Array{Float64}(undef,n);
-        dist_ymax = maximum(dist);
-        dist_xmax = length(dist)-1.0;
+        x = Array{Float64}(undef,n)
+        dist_ymax = maximum(dist)
+        dist_xmax = length(dist)-1.0
 
-        for i=1:n;
+        for i=1:n
             while true
                 # Pick random x value
-                rx = rand() * dist_xmax;
+                rx = rand() * dist_xmax
                 # Interpolate corresponding distribution value
                 f = floor(Int,rx)
                 y = dist[f+2]*(rx-f) + dist[f+1]*(1-(rx-f))
                 # See if x value is accepted
-                ry = rand() * dist_ymax;
+                ry = rand() * dist_ymax
                 if (y > ry)
-                    x[i] = rx / dist_xmax;
+                    x[i] = rx / dist_xmax
                     break
                 end
             end
@@ -144,20 +144,20 @@
     # by a vector of points defining the PDF curve
     function fill_from_distribution(dist::Array{Float64}, x::Array{Float64})
         # Fill the array x with random numbers from the distribution 'dist'
-        dist_ymax = maximum(dist);
-        dist_xmax = length(dist)-1.0;
+        dist_ymax = maximum(dist)
+        dist_xmax = length(dist)-1.0
 
-        for i=1:length(x);
+        for i=1:length(x)
             while true
                 # Pick random x value
-                rx = rand() * dist_xmax;
+                rx = rand() * dist_xmax
                 # Interpolate corresponding distribution value
                 f = floor(Int,rx)
                 y = dist[f+2]*(rx-f) + dist[f+1]*(1-(rx-f))
                 # See if x value is accepted
-                ry = rand() * dist_ymax;
+                ry = rand() * dist_ymax
                 if (y > ry)
-                    x[i] = rx / dist_xmax;
+                    x[i] = rx / dist_xmax
                     break
                 end
             end
@@ -175,9 +175,9 @@
         # p[3] = standard deviation
         # p[4] = sharpness
         # p[5] = skew
-        xs = (x .- p[2])./p[3]; # X scaled by mean and variance
-        v = 1/2 .- atan.(xs)./pi; # Sigmoid (positive on LHS)
-        f = p[1] .* exp.((p[4].^2).*(p[5].^2).*xs.*v .- (p[4].^2)./(p[5].^2).*xs.*(1 .- v));
+        xs = (x .- p[2])./p[3] # X scaled by mean and variance
+        v = 1/2 .- atan.(xs)./pi # Sigmoid (positive on LHS)
+        f = p[1] .* exp.((p[4].^2).*(p[5].^2).*xs.*v .- (p[4].^2)./(p[5].^2).*xs.*(1 .- v))
         return f
     end
 
@@ -189,9 +189,9 @@
         # p[3] = standard deviation
         # p[4] = sharpness
         # p[5] = skew
-        xs = (x-p[2,:])./p[3,:]; # X scaled by mean and variance
-        v = 1/2 .- atan.(xs)./pi; # Sigmoid (positive on LHS)
-        f = log.(p[1,:]) + (p[4,:].^2).*(p[5,:].^2).*xs.*v .- (p[4,:].^2)./(p[5,:].^2).*xs.*(1 .- v);
+        xs = (x-p[2,:])./p[3,:] # X scaled by mean and variance
+        v = 1/2 .- atan.(xs)./pi # Sigmoid (positive on LHS)
+        f = log.(p[1,:]) + (p[4,:].^2).*(p[5,:].^2).*xs.*v .- (p[4,:].^2)./(p[5,:].^2).*xs.*(1 .- v)
         return f
     end
 
@@ -203,9 +203,9 @@
     #     # p[3] = standard deviation
     #     # p[4] = sharpness
     #     # p[5] = skew
-    #     xs = (x-p[2])./p[3]; # X scaled by mean and variance
-    #     v = 1/2-atan.(xs)/pi; # Sigmoid (positive on LHS)
-    #     f = p[1] .* exp.(-(p[4].^2).*(p[5].^2).*(xs.^2).*v -(p[4].^2)./(p[5].^2).*(xs.^2).*(1-v));
+    #     xs = (x-p[2])./p[3] # X scaled by mean and variance
+    #     v = 1/2-atan.(xs)/pi # Sigmoid (positive on LHS)
+    #     f = p[1] .* exp.(-(p[4].^2).*(p[5].^2).*(xs.^2).*v -(p[4].^2)./(p[5].^2).*(xs.^2).*(1-v))
     #     return f
     # end
     #
@@ -217,9 +217,9 @@
     #     # p[3] = standard deviation
     #     # p[4] = sharpness
     #     # p[5] = skew
-    #     xs = (x-p[2,:])./p[3,:]; # X scaled by mean and variance
-    #     v = 1/2-atan.(xs)/pi; # Sigmoid (positive on LHS)
-    #     f = log.(p[1,:]) + (p[4,:].^2).*(p[5,:].^2).*(xs.^2).*v - (p[4,:].^2)./(p[5,:].^2).*(xs.^2).*(1-v);
+    #     xs = (x-p[2,:])./p[3,:] # X scaled by mean and variance
+    #     v = 1/2-atan.(xs)/pi # Sigmoid (positive on LHS)
+    #     f = log.(p[1,:]) + (p[4,:].^2).*(p[5,:].^2).*(xs.^2).*v - (p[4,:].^2)./(p[5,:].^2).*(xs.^2).*(1-v)
     #     return f
     # end
 
@@ -227,165 +227,165 @@
 
     # Returns a vector of bin centers if given a vector of bin edges
     function cntr(edges)
-        centers = (edges[1:end-1]+edges[2:end])/2;
-        return centers;
+        centers = (edges[1:end-1]+edges[2:end])/2
+        return centers
     end
 
     # Return percentile of an array along a specified dimension
     function pctile(A,p;dim=0)
-        s = size(A);
+        s = size(A)
         if dim==2
             out = Array{typeof(A[1])}(undef,s[1])
             for i=1:s[1]
                 t = .~ isnan.(A[i,:])
-                out[i] = percentile(A[i,t],p);
+                out[i] = percentile(A[i,t],p)
             end
         elseif dim==1
             out = Array{typeof(A[1])}(undef,s[2])
             for i=1:s[2]
                 t = .~ isnan.(A[:,i])
-                out[i] = percentile(A[t,i],p);
+                out[i] = percentile(A[t,i],p)
             end
         else
-            out = percentile(A,p);
+            out = percentile(A,p)
         end
         return out
     end
 
     function nanminimum(A;dim=0)
-        s = size(A);
+        s = size(A)
         if dim==2
             out = Array{typeof(A[1])}(undef,s[1])
             for i=1:s[1]
                 t = .~ isnan.(A[i,:])
-                out[i] = minimum(A[i,t]);
+                out[i] = minimum(A[i,t])
             end
         elseif dim==1
             out = Array{typeof(A[1])}(undef,s[2])
             for i=1:s[2]
                 t = .~ isnan.(A[:,i])
-                out[i] = minimum(A[t,i]);
+                out[i] = minimum(A[t,i])
             end
         else
             t = .~ isnan.(A)
-            out = minimum(A[t]);
+            out = minimum(A[t])
         end
         return out
     end
 
     function nanmaximum(A;dim=0)
-        s = size(A);
+        s = size(A)
         if dim==2
             out = Array{typeof(A[1])}(undef,s[1])
             for i=1:s[1]
                 t = .~ isnan.(A[i,:])
-                out[i] = maximum(A[i,t]);
+                out[i] = maximum(A[i,t])
             end
         elseif dim==1
             out = Array{typeof(A[1])}(undef,s[2])
             for i=1:s[2]
                 t = .~ isnan.(A[:,i])
-                out[i] = maximum(A[t,i]);
+                out[i] = maximum(A[t,i])
             end
         else
             t = .~ isnan.(A)
-            out = maximum(A[t]);
+            out = maximum(A[t])
         end
         return out
     end
 
     function nanrange(A;dim=0)
-        s = size(A);
+        s = size(A)
         if dim==2
             out = Array{typeof(A[1])}(undef,s[1])
             for i=1:s[1]
                 t = .~ isnan.(A[i,:])
-                extr = extrema(A[i,t]);
+                extr = extrema(A[i,t])
                 out[i] = extr[2] - extr[1]
             end
         elseif dim==1
             out = Array{typeof(A[1])}(undef,s[2])
             for i=1:s[2]
                 t = .~ isnan.(A[:,i])
-                extr = extrema(A[t,i]);
+                extr = extrema(A[t,i])
                 out[i] = extr[2] - extr[1]
             end
         else
             t = .~ isnan.(A)
             extr = extrema(A[t])
-            out = extr[2] - extr[1];
+            out = extr[2] - extr[1]
         end
         return out
     end
 
     function nanmean(A;dim=0)
-        s = size(A);
+        s = size(A)
         if dim==2
             out = Array{typeof(A[1])}(undef,s[1])
             for i=1:s[1]
                 t = .~ isnan.(A[i,:])
-                out[i] = mean(A[i,t]);
+                out[i] = mean(A[i,t])
             end
         elseif dim==1
             out = Array{typeof(A[1])}(undef,s[2])
             for i=1:s[2]
                 t = .~ isnan.(A[:,i])
-                out[i] = mean(A[t,i]);
+                out[i] = mean(A[t,i])
             end
         else
             t = .~ isnan.(A)
-            out = mean(A[t]);
+            out = mean(A[t])
         end
         return out
     end
 
     function nanstd(A;dim=0)
-        s = size(A);
+        s = size(A)
         if dim==2
             out = Array{typeof(A[1])}(undef,s[1])
             for i=1:s[1]
                 t = .~ isnan.(A[i,:])
-                out[i] = std(A[i,t]);
+                out[i] = std(A[i,t])
             end
         elseif dim==1
             out = Array{typeof(A[1])}(undef,s[2])
             for i=1:s[2]
                 t = .~ isnan.(A[:,i])
-                out[i] = std(A[t,i]);
+                out[i] = std(A[t,i])
             end
         else
             t = .~ isnan.(A)
-            out = std(A[t]);
+            out = std(A[t])
         end
         return out
     end
 
     function nanmedian(A;dim=0)
-        s = size(A);
+        s = size(A)
         if dim==2
             out = Array{typeof(A[1])}(undef,s[1])
             for i=1:s[1]
                 t = .~ isnan.(A[i,:])
-                out[i] = median(A[i,t]);
+                out[i] = median(A[i,t])
             end
         elseif dim==1
             out = Array{typeof(A[1])}(undef,s[2])
             for i=1:s[2]
                 t = .~ isnan.(A[:,i])
-                out[i] = median(A[t,i]);
+                out[i] = median(A[t,i])
             end
         else
             t = .~ isnan.(A)
-            out = median(A[t]);
+            out = median(A[t])
         end
         return out
     end
 
     # Return the index of the closest value of Target for each value in Source
     function findclosest(source, target)
-        index=Array{Int64}(undef,size(source));
+        index=Array{Int64}(undef,size(source))
         for i=1:length(source)
-            index[i]=argmin((target .- source[i]).^2);
+            index[i]=argmin((target .- source[i]).^2)
         end
         return index
     end
@@ -393,25 +393,25 @@
     # Return the index of the closest value of the vector 'target' below each
     # value in 'source'
     function findclosestbelow(source, target)
-        index=Array{Int64}(undef,size(source));
+        index=Array{Int64}(undef,size(source))
         for i=1:length(source)
             t = findall(target .< source[i])
-            ti = argmin((target[t] .- source[i]).^2);
-            index[i] = t[ti];
+            ti = argmin((target[t] .- source[i]).^2)
+            index[i] = t[ti]
         end
-        return index;
+        return index
     end
 
     # Return the index of the closest value of the vector 'target' above each
     # value in 'source'
     function findclosestabove(source, target)
-        index=Array{Int64}(undef,size(source));
+        index=Array{Int64}(undef,size(source))
         for i=1:length(source)
             t = findall(target .> source[i])
-            ti = argmin((target[t] .- source[i]).^2);
-            index[i] = t[ti];
+            ti = argmin((target[t] .- source[i]).^2)
+            index[i] = t[ti]
         end
-        return index;
+        return index
     end
 
 
