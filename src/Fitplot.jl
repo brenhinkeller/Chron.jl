ced9a6173348243d1842f14b432e3f911ea236b9
C. Brenhin Keller <cbkeller@berkeley.edu>
2019-08-06 02:42:18 -0400

    handle colors in rankorder plots

--- src/Fitplot.jl
+++ src/Fitplot.jl
@@ -1,22 +1,25 @@
 ## --- Utility functions for plotting
 
-    function plot_rankorder_errorbar(data,uncert; seriestype=:scatter,ylabel="",label="",xticks=[],xlabel="")
+    function plot_rankorder_errorbar(data,uncert; seriestype=:scatter,ylabel="",label="",xticks=[],xlabel="",color=:auto)
         sI = sortperm(data)
         h = plot(1:length(sI),data[sI],yerror=uncert[sI],seriestype=seriestype,
-                 markerstrokecolor=:auto,label=label,ylabel=ylabel,xlabel=xlabel,xticks=xticks)
+                markerstrokecolor=:auto,color=color,label=label,ylabel=ylabel,
+                xlabel=xlabel, xticks=xticks)
         return h
     end
 
-    function plot_rankorder_errorbar!(h::Plots.Plot,data,uncert,i0; seriestype=:scatter,ylabel="",label="",xticks=[],xlabel="")
+    function plot_rankorder_errorbar!(h::Plots.Plot,data,uncert,i0; seriestype=:scatter,ylabel="",label="",xticks=[],xlabel="",color=:auto)
         sI = sortperm(data)
         plot!(h, i0 .+ (1:length(sI)),data[sI],yerror=uncert[sI],seriestype=seriestype,
-                 markerstrokecolor=:auto,label=label,ylabel=ylabel,xlabel=xlabel,xticks=xticks)
+                 markerstrokecolor=:auto,color=color,label=label,ylabel=ylabel,
+                 xlabel=xlabel,xticks=xticks)
     end
 
-    function plot_rankorder_errorbar!(data,uncert,i0; seriestype=:scatter,ylabel="",label="",xticks=[],xlabel="")
+    function plot_rankorder_errorbar!(data,uncert,i0; seriestype=:scatter,ylabel="",label="",xticks=[],xlabel="",color=:auto)
         sI = sortperm(data)
         plot!(i0 .+ (1:length(sI)),data[sI],yerror=uncert[sI],seriestype=seriestype,
-                 markerstrokecolor=:auto,label=label,ylabel=ylabel,xlabel=xlabel,xticks=xticks)
+                 markerstrokecolor=:auto,color=color,label=label,ylabel=ylabel,
+                 xlabel=xlabel,xticks=xticks)
     end
 
 
