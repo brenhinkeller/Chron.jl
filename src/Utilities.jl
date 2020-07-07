e58509cc2140403afc9a0f6140663e609c0487fa
C. Brenhin Keller <cbkeller@berkeley.edu>
2020-07-07 15:03:19 -0400

    Port documented and more granularly-exported legacy functions from statgeochem

--- src/Utilities.jl
+++ src/Utilities.jl
@@ -924,31 +924,77 @@
 
 ## --- Other
 
-    # Direct system() access without stripping special characters like Julia's
-    # rather protective "run()".
-    function system(cmdstr)
+    """
+    ```julia
+    system(cmdstr::AbstractString)
+    ```
+
+    Direct access to the command line through C's `system` function -- without
+    stripping/sanitizing special characters,  in contrast to Julia's safer
+    `run()` function. This allows pipelining, etc. in shell commands. Returns
+    0 on success.
+    """
+    function system(cmdstr::AbstractString)
         return ccall((:system,), Int, (Cstring,), cmdstr)
     end
     export system
 
-    if VERSION>=v"1.0"
+## --- Retain deprecated functions with matlab-like syntax, to avoid breakages in user scripts that may depend on them
+
+    if ~ @isdefined linspace
+        """
+        ```julia
+        linspace(l::Number,u::Number,n::Number)
+        ```
+
+        Returns a linearly spaced array with `n` points between the starting
+        bound `l` and ending bound `u`
+        """
         function linspace(l::Number,u::Number,n::Number)
             return range(l,stop=u,length=n)
         end
         export linspace
+    end
 
-        function repmat(A::AbstractArray, vert::Integer)
-            return repeat(A, outer=vert)
-        end
-        function repmat(A::AbstractArray, vert::Integer, horiz::Integer)
-            return repeat(A, outer=(vert, horiz))
-        end
-        export repmat
+    if ~ @isdefined linreg
+        """
+        ```julia
+        (a,b) = linreg(x::AbstractVector, y::AbstractVector)
+        ```
 
-        function contains(haystack::AbstractString, needle::Union{AbstractString,Regex,AbstractChar})
-            return occursin(needle::Union{AbstractString,Regex,AbstractChar}, haystack::AbstractString)
-        end
+        Returns the coefficients for a simple linear least-squares regression of
+        the form `y = a + bx`
+        """
+        linreg(x::AbstractVector, y::AbstractVector) = hcat(fill!(similar(x), 1), x) \ y
+        export linreg
+    end
+
+    if ~ @isdefined contains
+        """
+        ```julia
+        contains(haystack, needle)
+        ```
+
+        Converts both `haystack` and `needle` to strings (if not already strings)
+        and checks whether `string(haystack)` contains `string(needle)`.
+        """
+        contains(haystack::AbstractString, needle::Union{AbstractString,Regex,AbstractChar}) = occursin(needle, haystack)
+        contains(haystack, needle) = occursin(string(needle), string(haystack))
         export contains
+    end
+
+    if ~ @isdefined containsi
+        """
+        ```julia
+        containsi(haystack, needle)
+        ```
+
+        Converts both `haystack` and `needle` to strings and checks whether
+        `string(haystack)` contains `string(needle)`, ignoring case.
+        """
+        containsi(haystack::AbstractString, needle::Union{AbstractString,AbstractChar}) = occursin(lowercase(needle), lowercase(haystack))
+        containsi(haystack, needle) = occursin(lowercase(string(needle)), lowercase(string(haystack)))
+        export containsi
     end
 
 ## --- End of File
