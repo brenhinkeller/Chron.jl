0d66a1d12966bae4df50317770cfdcdb716561d7
C. Brenhin Keller <cbkeller@berkeley.edu>
2020-07-17 02:30:38 -0400

    More complete and faster tests for DistMetropolis functions

--- test/testDist.jl
+++ test/testDist.jl
@@ -1,1 +1,5 @@
-@test all(isapprox.(nanmean.(metropolis_minmax(10^6, MeltsVolcanicZirconDistribution, collect(100:0.1:101), 0.01*ones(11), burnin=10^5))[1:2], (99.92,101.08), atol=0.01))
+
+mu = collect(100:0.1:101); sigma = 0.01*ones(11);
+@test Chron.check_dist_ll(MeltsVolcanicZirconDistribution, mu, sigma, 100,101) ≈ -3.6933357793356576
+@test isapprox.(nanmean.(metropolis_minmax(2*10^5, MeltsVolcanicZirconDistribution, mu, sigma, burnin=10^5))[1:2], (99.92,101.08), atol=0.015) |> all
+@test isapprox(nanmean(metropolis_min(2*10^5, MeltsVolcanicZirconDistribution, mu, sigma, burnin=10^5)), 99.9228, atol=0.015)
