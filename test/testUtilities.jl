1797076755e0a53c658de66d8e3f058ae0591ff7
C.Brenhin Keller <cbkeller@dartmouth.edu>
2021-02-13 19:25:53 -0500

    Test colormaps

--- test/testUtilities.jl
+++ test/testUtilities.jl
@@ -185,3 +185,19 @@
     @test trapz(1:10,fill(1,10)) == 9
     @test trapz(collect(1:10.),ones(10)) == 9
     @test midpointintegrate(1:10,ones(10)) == 10
+
+
+# Colormaps
+    using Colors: Color
+    cmap = resize_colormap(viridis, 10)
+    @test length(cmap) == 10
+    @test isa(cmap, Array{<:Color,1})
+
+    matrix = rand(10,10)
+    # Specifiying limits
+    img1 = imsc(matrix, viridis, 0, 1)
+    @test isa(img1, Array{<:Color,2})
+
+    # Auto-ranging
+    img1 = imsc(matrix, viridis)
+    @test isa(img1, Array{<:Color,2})
