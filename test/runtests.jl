7d3284dcd2dff6c8dbdd787f01f2fc9eea6b29c8
C.Brenhin Keller <cbkeller@dartmouth.edu>
2021-06-03 22:27:09 -0400

    remove extraneous stuff from gh-pages branch

--- test/runtests.jl
+++
@@ -1,8 +0,0 @@
-using Chron
-using Test, Statistics, StatsBase
-
-include("testUtilities.jl")
-include("testDist.jl")
-include("testStratOnly.jl")
-include("testRadiocarbon.jl")
-include("testCoupled.jl")
