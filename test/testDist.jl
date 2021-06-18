1fd50330dbe2e3a14342e97bc8416a9930388aa9
C.Brenhin Keller <cbkeller@dartmouth.edu>
2021-06-17 20:08:24 -0400

    test tmin and tmax separately

--- test/testDist.jl
+++ test/testDist.jl
@@ -2,8 +2,12 @@
 
     mu = collect(100:0.1:101); sigma = 0.01*ones(11);
     @test Chron.check_dist_ll(MeltsVolcanicZirconDistribution, mu, sigma, 100,101) ≈ -3.6933357793356576
-    @test isapprox.(nanmean.(metropolis_minmax(2*10^5, MeltsVolcanicZirconDistribution, mu, sigma, burnin=10^5))[1:2], (99.92,101.08), atol=0.015) |> all
+
+    tmindist, tmaxdist, lldist, acceptancedist = metropolis_minmax(2*10^5, MeltsVolcanicZirconDistribution, mu, sigma, burnin=10^5)
+    @test isapprox(nanmean(tmindist), 99.92, atol=0.015)
+    @test isapprox(nanmean(tmaxdist), 101.08, atol=0.015)
     @test isapprox(nanmean(metropolis_min(2*10^5, MeltsVolcanicZirconDistribution, mu, sigma, burnin=10^5)), 99.9228, atol=0.015)
+
 
 ## -- Test distribution bootstrapping
 
