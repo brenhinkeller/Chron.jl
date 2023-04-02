f2db6c19fbb6f06a4891b489bc73b76007642993
C. Brenhin Keller <cbkeller@dartmouth.edu>
2023-04-02 13:37:16 -0400

    Update distmetropolis docstrings

--- src/DistMetropolis.jl
+++ src/DistMetropolis.jl
@@ -2,11 +2,18 @@
 
     """
     ```julia
-    BootstrapCrystDistributionKDE(smpl::ChronAgeData; cutoff=-0.05)
+    BootstrapCrystDistributionKDE(smpl::ChronAgeData; cutoff=-0.05, [tpbloss=0])
     ```
     Bootstrap an estimate of the pre-eruptive (or pre-depositional) mineral
     crystallization distribution shape from a Chron.ChronAgeData object containing
     data for several samples, using a kernel density estimate of stacked sample data.
+
+    If the samples provided as csv files in `smpl.Path` take the five-column form
+    of U-Pb isotopic data files, the ages and uncertainties will be those of upper
+    intercepts given a time of Pb-loss optionally specified as `tpbloss`.
+
+    Uncertainties will be treated as one or two-sigma absolute based on the value
+    of `smpl.inputSigmaLevel`.
 
     ### Examples
     ```julia
--- src/Fitplot.jl
+++ src/Fitplot.jl
@@ -72,15 +72,30 @@
     tMinDistMetropolis(smpl::ChronAgeData, nsteps::Int, burnin::Int, dist::Array{Float64})
     ```
     Calculate the minimum limiting (eruption/deposition) age of each sample defined
-    in the `smpl` struct, using the `metropolis_min` function, assuming mineral
+    in the `smpl` struct, using the `Isoplot.metropolis_min` function, assuming mineral
     ages for each sample are drawn from the source distribution `dist`. Fits a
     `bilinear_exponential` function to the resulting stationary distribution
     for each sample and stores the results in `smpl.Params` for use by the
     `StratMetropolisDist` function.
 
+    If the samples provided as csv files in `smpl.Path` take the five-column form
+    of U-Pb isotopic data files, these will be interpreted as the columns
+    ```
+    | ²⁰⁷Pb/²³⁵U | uncert (absolute) | ²⁰⁶Pb/²³⁸U | uncert (absolute) | correlation |
+    ```
+    and Pb-loss-aware eruption/deposition age estimation will be conducted. In
+    all other cases, the first two columns of each data file will be interpreted as
+    ```
+    | Age | Age uncert (absolute) |
+    ```
+    and standard eruption/deposition age estimation will be conducted.
+
+    In all cases, uncertainties will be treated as one or two-sigma _absolute_
+    based on the value of `smpl.inputSigmaLevel`.
+
     ### Examples
     ```julia
-    smpl = tMinDistMetropolis(smpl, 5*10^5, 2*10^5, TriangularDistribution)
+    smpl = tMinDistMetropolis(smpl, 10^6, 10^5, HalfNormalDistribution)
     ```
     """
     function tMinDistMetropolis(smpl::ChronAgeData,nsteps::Int,burnin::Int,dist::DenseArray{Float64};
